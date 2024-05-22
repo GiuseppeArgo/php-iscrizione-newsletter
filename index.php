ab7bf0550cab5613500727dee630ae32da835cc2
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-05-22 18:01:35 +0200

    feat: complete milestone 4

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
 require_once __DIR__ . "/partials/functions.php";
 
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+$wrong_mail = "";
+$result = false;
+
 if (isset($_POST["user_email"])) {
     $user_email = $_POST["user_email"];
     $result = check_email($user_email);
-}
+
+    if ($result) {
+        $_SESSION["auth"] = true;
+        header("Location: ./thankyou.php");
+    } else {
+        $error = "L'email non è valida, assicurati che contenga '.' e '@'";
+    }
+} 
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +26,7 @@ if (isset($_POST["user_email"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-iscrizione-newsletter</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -25,11 +39,11 @@ if (isset($_POST["user_email"])) {
                 <label for="mail" class="form-label">Inserisci la tua mail</label>
                 <input type="text" class="form-control" id="mail" name="user_email">
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-secondary">Submit</button>
             </form>
-            <?php if (isset($result)) { ?>
-                <div class="p-3 w-50 mx-auto my-3 text-center <?php echo $result ? "bg-success-subtle" : "bg-danger-subtle" ?>">
-                    <?php echo $result ? "Iscrizione effettuata" : "L'email non è valida, assicurati che contenga '.' e '@'" ?>
+            <?php if (isset($error)) { ?>
+                <div class="p-3 w-50 mx-auto mt-3 text-center bg-danger-subtle">
+                    <?php echo $error ?>
                 </div>
             <?php } ?>
         </div>
---
+++ thankyou.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-iscrizione-newsletter-thankyou</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> 
+</head>
+<body>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-6">
+            <h1 class="text-center mb-5">Grazie per esserti iscritto!</h1>
+            <div class="p-3 w-50 mx-auto mt-3 text-center bg-success-subtle">
+                <p>Iscrizione Effetuata</p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
